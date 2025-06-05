5dd655473f7b2a063028f407b33e054d292b31d4
Park DongSu <120581232+gold-su@users.noreply.github.com>
2025-06-05 11:11:32 +0900

    Update BookDao.java

--- BookRentalProject/src/main/java/com/office/library/book/admin/BookDao.java
+++ BookRentalProject/src/main/java/com/office/library/book/admin/BookDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper; // ✅ 올바른 RowMapper
+import org.springframework.jdbc.core.RowMapper; 
 import org.springframework.stereotype.Component;
 
 import com.office.library.book.BookVo;
